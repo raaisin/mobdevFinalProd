4aefe5d1e91e52d98e8abd500cded9f72266401c
Ardel Tioco Jeff Lauron <ardeltiocojefflauron@gmail.com>
2024-05-10 18:20:48 +0800

    may 10 changes well nag add rako ug welcome page

--- app/src/main/java/com/example/mobdevfinalprod/WelcomePage.java
+++ app/src/main/java/com/example/mobdevfinalprod/WelcomePage.java
@@ -10,13 +10,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 
 public class WelcomePage extends AppCompatActivity {
-    private FrameLayout framelayout;
+    private LinearLayout framelayout;
     public static RegistrationPage registrationPage;
     public static LoginPage loginPage;
+    private ImageButton getting_started;
+    private TextView already_have;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +32,21 @@ public class WelcomePage extends AppCompatActivity {
         loginPage = new LoginPage();
         framelayout = findViewById(R.id.frame_layout);
 
-        getSupportFragmentManager().beginTransaction().add(R.id.frame_layout, loginPage).commit();
+        getting_started = findViewById(R.id.welcome_register_button);
+        getting_started.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().add(R.id.frame_layout, registrationPage).commit();
+            }
+        });
+
+        already_have = findViewById(R.id.go_to_login);
+        already_have.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().add(R.id.frame_layout, loginPage).commit();
+            }
+        });
     }
     public static void changeToRegistration(FragmentManager fragmentManager, RegistrationPage registrationPage) {
         FragmentTransaction transaction = fragmentManager.beginTransaction();
