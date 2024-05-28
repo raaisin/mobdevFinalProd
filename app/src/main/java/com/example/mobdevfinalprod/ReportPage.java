6d1f4e66330f8b323697617f1cd0e4e101e2bfde
raaisin <russellpalma4554@gmail.com>
2024-05-28 16:14:30 +0800

    initial commit

--- app/src/main/java/com/example/mobdevfinalprod/ReportPage.java
+++ app/src/main/java/com/example/mobdevfinalprod/ReportPage.java
@@ -7,6 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -54,23 +58,29 @@ public class ReportPage extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
 
-
-
-
     }
 
 
     public double calculateCalorie(double height, double weight, int age, boolean gender){
 
         double BMR;
-        if(gender){
-            BMR = (10*weight) + (6.25*height) - (double)(5*age) + 5;
-        }
-        else{
-            BMR = (10*weight) + (6.25*height) - (double)(5*age) - 161;
+        if (gender) {
+            BMR = (10.0 * weight) + (6.25 * height) - (5.0 * age) - 161.0;
+        } else {
+            BMR = (10.0 * weight) + (6.25 * height) - (5.0 * age) + 5.0;
         }
 
         return BMR;
+
+    }
+
+    public double calculateBMI(double height, double weight){
+
+        double BMI;
+
+        BMI = (weight)/((height*100) * (height*100));
+
+        return BMI;
 
     }
 
@@ -79,6 +89,74 @@ public class ReportPage extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_report_page, container, false);
+
+
+
+            EditText heightBMI = view.findViewById(R.id.editHeightBMI);
+            EditText weightBMI = view.findViewById(R.id.editWeightBMI);
+            EditText resultBMI = view.findViewById(R.id.editResultBMI);
+
+            EditText heightCalorie = view.findViewById(R.id.editHeightCalorie);
+            EditText weightCalorie = view.findViewById(R.id.editWeightCalorie);
+
+            EditText ageCalorie = view.findViewById(R.id.editAgeCalorie);
+
+            RadioButton rMale = view.findViewById(R.id.radioMale);
+            RadioButton rFemale = view.findViewById(R.id.radioFemale);
+
+            RadioButton rFirst = view.findViewById(R.id.radioFit1);
+            RadioButton rSecond = view.findViewById(R.id.radioFit2);
+            RadioButton rThird = view.findViewById(R.id.radioFit3);
+            RadioButton rFourth = view.findViewById(R.id.radioFit4);
+            RadioButton rFifth = view.findViewById(R.id.radioFit5);
+
+            TextView tvModerate = view.findViewById(R.id.TVModerate);
+            TextView tvMild = view.findViewById(R.id.TVMild);
+            TextView tvNormal = view.findViewById(R.id.TVNormal);
+            TextView tvExtreme = view.findViewById(R.id.TVExtreme);
+
+
+            if (heightBMI != null && !heightBMI.getText().toString().isEmpty()) {
+                double BMIHeight = Double.parseDouble(heightBMI.getText().toString());
+                double BMIWeight = Double.parseDouble(weightBMI.getText().toString());
+
+                resultBMI.setText(String.valueOf(calculateBMI(BMIHeight, BMIWeight)));
+            }
+
+
+
+
+
+            int age = Integer.parseInt(ageCalorie.getText().toString());
+
+            boolean gender;
+            if(rMale.isChecked()){
+                gender = false;
+            }
+            if(rFemale.isChecked()){
+                gender = true;
+            }
+
+
+            double multiplier;
+            if(rFirst.isChecked()){
+                multiplier = 0.20;
+            }
+            else if (rSecond.isChecked()) {
+                multiplier = 0.38;
+            }
+            else if (rThird.isChecked()) {
+                multiplier = 0.47;
+            }
+            else if (rFourth.isChecked()) {
+                multiplier = 0.55;
+            }
+            else if (rFifth.isChecked()) {
+                multiplier = 0.73;
+            }
+
+
+        return view;
     }
 }
