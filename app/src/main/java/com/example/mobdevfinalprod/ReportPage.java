80975ec8f961ff9c0d5835482f23f6ebb57224d9
Ardel Tioco Jeff Lauron <ardeltiocojefflauron@gmail.com>
2024-05-28 18:23:49 +0800

    Health Insights started

--- app/src/main/java/com/example/mobdevfinalprod/ReportPage.java
+++ app/src/main/java/com/example/mobdevfinalprod/ReportPage.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
 import android.widget.Toast;
 
 /**
@@ -62,6 +63,7 @@ public class ReportPage extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_report_page, container, false);
         addGenderButtonFunction(view);
+
         return view;
     }
     private void addGenderButtonFunction(View view) {
